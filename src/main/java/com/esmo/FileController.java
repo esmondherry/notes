b312ef20e41c1fd4ecb4f9c66838f0676a349612
esmond <112294683+esmondherry@users.noreply.github.com>
2023-06-30 21:57:14 -0400

    Add some javadoxs

--- src/main/java/com/esmo/FileController.java
+++ src/main/java/com/esmo/FileController.java
@@ -10,6 +10,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileController {
+    /**
+     * Read a files contents
+     * 
+     * @param filePath path to the file
+     * @return the content of the file as a string
+     * @throws IOException if an IO error occurs
+     */
     public static String readFile(String filePath) throws IOException {
         StringBuilder content = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -21,6 +28,13 @@ public class FileController {
         return content.toString();
     }
 
+    /**
+     * Save a string to file
+     * 
+     * @param filePath the path to the file to save to
+     * @param data     the <code>String</code> to be written to the file
+     * @throws IOException if an IO error occurs
+     */
     public static void saveFile(String filePath, String data) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
             writer.write(data);
@@ -36,6 +50,13 @@ public class FileController {
         file.createNewFile();
     }
 
+    /**
+     * Changes the name of the file
+     * 
+     * @param filePath the path to the file
+     * @param newName  the new name of the file
+     * @return <code>true</code> if the renaming was successful;
+     */
     public static boolean changeFileName(String filePath, String newName) {
         File file = new File(filePath);
         File newFile = new File(file.getParent(), newName);
