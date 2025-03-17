382e5cac4611092dd6b5f9203f30b2c983f1be0e
esmond <112294683+esmondherry@users.noreply.github.com>
2025-03-17 16:38:19 -0400

    build ui

--- src/main/java/com/esmo/view/AppView.java
+++ src/main/java/com/esmo/view/AppView.java
@@ -1,5 +1,6 @@
 package com.esmo.view;
 
+import javafx.geometry.Orientation;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SplitPane;
@@ -21,41 +22,29 @@ public class AppView {
     private Button saveButton;
     private Button deleteButton;
     private Button settingsButton;
+    private ListView<String> tagListView;
+    private TextField addTagField;
+    private Button addTagButton;
+    private HBox addTagBox;
+    private Button removeTag;
 
     public AppView() {
 
-        nameField = new TextField();
-        nameButton = new Button("Change");
-        HBox nameBox = new HBox(nameField, nameButton);
+        VBox textPane = createTextPane();
 
-        textArea = new TextArea();
-        VBox.setVgrow(textArea, Priority.ALWAYS);
+        VBox filesPane = createFilesPane();
 
-        VBox textPane = new VBox(nameBox, textArea);
+        VBox tagPane = createTagPane();
 
-        searchField = new TextField();
-        searchField.setPromptText("Search...");
-        HBox.setHgrow(searchField, Priority.ALWAYS);
-        HBox searchBox = new HBox(searchField);
+        SplitPane sidePane = new SplitPane(filesPane, tagPane);
+        sidePane.setOrientation(Orientation.VERTICAL);
+        sidePane.setDividerPositions(.7);
 
-        listView = new ListView<>();
-        listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            nameField.setText(newValue);
-        });
-        VBox.setVgrow(listView, Priority.ALWAYS);
-
-        VBox filesPane = new VBox(searchBox, listView);
-
-        SplitPane splitPane = new SplitPane(textPane, filesPane);
+        SplitPane splitPane = new SplitPane(textPane, sidePane);
         splitPane.setDividerPositions(0.7);
         VBox.setVgrow(splitPane, Priority.ALWAYS);
 
-        ToolBar toolbar = new ToolBar();
-        newButton = new Button("New");
-        saveButton = new Button("Save");
-        deleteButton = new Button("Delete");
-        settingsButton = new Button("Settings");
-        toolbar.getItems().addAll(newButton, saveButton, deleteButton, settingsButton);
+        ToolBar toolbar = createToolbar();
 
         pane = new VBox(splitPane, toolbar);
     }
@@ -100,4 +89,70 @@ public class AppView {
         return settingsButton;
     }
 
+    public ListView<String> getTagListView() {
+        return tagListView;
+    }
+
+    public TextField getAddTagField() {
+        return addTagField;
+    }
+
+    public Button getAddTagButton() {
+        return addTagButton;
+    }
+
+    public HBox getAddTagBox() {
+        return addTagBox;
+    }
+
+    public Button getRemoveTag() {
+        return removeTag;
+    }
+
+    private ToolBar createToolbar() {
+        ToolBar toolbar = new ToolBar();
+        newButton = new Button("New");
+        saveButton = new Button("Save");
+        deleteButton = new Button("Delete");
+        settingsButton = new Button("Settings");
+        toolbar.getItems().addAll(newButton, saveButton, deleteButton, settingsButton);
+        return toolbar;
+    }
+
+    private VBox createFilesPane() {
+        listView = new ListView<>();
+        searchField = new TextField();
+        searchField.setPromptText("Search...");
+
+        HBox.setHgrow(searchField, Priority.ALWAYS);
+        Button tagfilters = new Button("V");
+
+        HBox searchBox = new HBox(searchField, tagfilters);
+        VBox.setVgrow(listView, Priority.ALWAYS);
+
+        VBox filesPane = new VBox(searchBox, listView);
+        return filesPane;
+    }
+
+    private VBox createTextPane() {
+        nameField = new TextField();
+        nameButton = new Button("Change");
+        HBox nameBox = new HBox(nameField, nameButton);
+
+        textArea = new TextArea();
+        VBox.setVgrow(textArea, Priority.ALWAYS);
+
+        VBox textPane = new VBox(nameBox, textArea);
+        return textPane;
+    }
+
+    private VBox createTagPane() {
+        tagListView = new ListView<>();
+        addTagField = new TextField();
+        addTagButton = new Button("Add Tag");
+        addTagBox = new HBox(addTagField, addTagButton);
+        removeTag = new Button("Remove Selected Tag");
+        removeTag.setMaxWidth(Double.MAX_VALUE);
+        return new VBox(tagListView, removeTag, addTagBox);
+    }
 }
